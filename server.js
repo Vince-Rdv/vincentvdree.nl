30dddd912a042c272f817c660883814b2ee941de
Vince-Rdv <vincent15vanderree@gmail.com>
2022-01-27 18:39:07 +0100

    Final quick fix

--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-const { MongoClient } = require('mongodb');
-
-const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
-
 const express = require('express')
 const app = express()
 const port = 3000
