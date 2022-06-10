32811fcdb93fdb77a55e13c5b9ec24d1ac712d9f
Vince-Rdv <vincent15vanderree@gmail.com>
2022-06-10 16:52:34 +0200

    Update

---
+++ public/so-what.nl/index.js
@@ -0,0 +1,10 @@
+var navigationBar = document.getElementById("navigationBar");
+var navigationBarButton = document.getElementById("navigationBarButton");
+
+function navigationBarSwitch() {
+    if (navigationBar.style.display == "block") {
+        navigationBar.style.display = "none";
+    } else {
+        navigationBar.style.display = "block";
+    }
+};
